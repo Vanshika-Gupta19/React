3e0b3f16cd5f0ec3faa5122b1a270c40b483f59f
Vanshika-Gupta19 <vanshika2004.gupta19@gmail.com>
2024-06-17 13:25:41 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useRef} from 'react';
 
 function App() {
+
+  const[count, setCount] = useState(0);
+  // let a = 0;
+  const a = useRef(0)
+  const btnref = useRef()
+
+  useEffect(() => {
+    // a = a + 1;
+    a.current = a.current + 1;
+    console.log(`rerendering and the value of a is ${a.current}...`)
+    btnref.current.style.backgroundColor = "skyblue"
+  })
+  
+
   return (
+    <>
     <div className="App">
       <header className="App-header">
+
+      <button ref={btnref} onClick = {() => setCount((count)=> count+1)}> count is {count} </button>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +38,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
